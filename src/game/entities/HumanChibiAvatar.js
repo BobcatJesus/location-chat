e3699de5bf4fb6b515e52215919ed391c097dcef
unknown <you@example.com>
2026-08-13 17:54:59 -0500

    Match hoodie walk-style avatar reference

--- src/game/entities/HumanChibiAvatar.js
+++ src/game/entities/HumanChibiAvatar.js
@@ -25,99 +25,120 @@ function ensureChibiFrame(scene, key, palette, direction = 'front', step = 0) {
   const h = 96;
 
   const skin = palette.skin;
-  const skinShade = tintInt(skin, -20);
-  const skinGlow = tintInt(skin, 18);
+  const skinShade = tintInt(skin, -18);
   const hair = palette.hair;
-  const shirt = 0xf6f1e9;
-  const shirtShade = 0xe7dece;
-  const pants = tintInt(palette.outfit, -56);
-  const pantsShade = tintInt(pants, -18);
+  const hoodie = tintInt(palette.outfit, 18);
+  const hoodieShade = tintInt(hoodie, -26);
+  const shorts = tintInt(palette.outfit, -54);
+  const shortsShade = tintInt(shorts, -16);
+  const belt = 0xc9b54e;
   const outline = 0x3b2f30;
 
   const stepX = step === 0 ? 0 : 1;
-  const legOffset = step === 0 ? 0 : 2;
-  const armSwing = step === 0 ? 0 : 1;
+  const legOffset = step === 0 ? 0 : 3;
+  const armSwing = step === 0 ? 0 : 2;
+
+  const facingSide = direction === 'side';
+  const facingBack = direction === 'back';
 
   // Soft floor shadow.
-  g.fillStyle(0x000000, 0.16);
-  g.fillEllipse(48, 85, 28, 8);
+  g.fillStyle(0x000000, 0.18);
+  g.fillRoundedRect(24, 86, 48, 4, 2);
 
-  // Head + ears + neck.
+  // Head + ears.
   g.fillStyle(skin, 1);
-  g.fillCircle(48, 29, 21);
-  g.fillCircle(30, 35, 6);
-  g.fillCircle(66, 35, 6);
-  g.fillRoundedRect(44, 45, 8, 6, 3);
-
-  g.fillStyle(skinGlow, 0.24);
-  g.fillEllipse(48, 30, 26, 18);
-
-  // Hair cap.
-  g.fillStyle(hair, 1);
-  g.fillEllipse(48, 22, 44, 26);
-  if (direction === 'back') {
-    g.fillRoundedRect(31, 20, 34, 12, 6);
-  } else if (direction === 'front') {
-    g.fillRoundedRect(33, 23, 30, 8, 4);
-  } else if (direction === 'side') {
-    g.fillRoundedRect(38, 22, 24, 10, 4);
+  g.fillCircle(48, 30, 21);
+  if (!facingBack) {
+    g.fillCircle(facingSide ? 65 : 30, 36, 6);
+    if (!facingSide) g.fillCircle(66, 36, 6);
   }
 
-  // Shirt body.
-  g.fillStyle(shirt, 1);
-  g.fillRoundedRect(33, 48, 30, 24, 8);
-  g.fillStyle(shirtShade, 0.64);
-  g.fillRoundedRect(36, 58, 24, 10, 6);
+  // Hair cap + bun.
+  g.fillStyle(hair, 1);
+  g.fillEllipse(48, 23, 44, 26);
+  if (facingBack) {
+    g.fillRoundedRect(31, 21, 34, 12, 6);
+    g.fillCircle(52, 10, 6);
+  } else if (facingSide) {
+    g.fillRoundedRect(35, 22, 30, 11, 5);
+    g.fillCircle(58, 10, 6);
+  } else {
+    g.fillRoundedRect(33, 24, 30, 8, 4);
+    g.fillCircle(52, 10, 6);
+  }
 
-  // Collar.
-  g.fillStyle(0xffffff, 0.9);
-  g.fillRoundedRect(42, 48, 12, 4, 2);
+  // Hoodie torso.
+  g.fillStyle(hoodie, 1);
+  g.fillRoundedRect(32, 49, 32, 23, 8);
+  g.fillStyle(hoodieShade, 0.42);
+  g.fillRoundedRect(36, 61, 24, 8, 5);
+
+  // Hood (front/back/side variants).
+  g.fillStyle(hoodieShade, 1);
+  if (facingBack) {
+    g.fillRoundedRect(39, 43, 18, 10, 5);
+  } else if (facingSide) {
+    g.fillRoundedRect(41, 44, 18, 9, 5);
+  } else {
+    g.fillRoundedRect(40, 45, 16, 8, 4);
+  }
 
   // Arms.
-  g.fillStyle(shirt, 1);
-  g.fillRoundedRect(27 + armSwing, 50, 8, 21, 4);
-  g.fillRoundedRect(61 - armSwing, 50, 8, 21, 4);
+  g.fillStyle(hoodie, 1);
+  g.fillRoundedRect(26 + armSwing, 51, 8, 20, 4);
+  g.fillRoundedRect(62 - armSwing, 51, 8, 20, 4);
   g.fillStyle(skinShade, 1);
-  g.fillCircle(31 + armSwing, 73, 4);
-  g.fillCircle(65 - armSwing, 73, 4);
+  g.fillCircle(30 + armSwing, 72, 4);
+  g.fillCircle(66 - armSwing, 72, 4);
+
+  // Waist band.
+  g.fillStyle(belt, 1);
+  g.fillRoundedRect(34, 69, 28, 5, 2);
+
+  // Shorts.
+  g.fillStyle(shorts, 1);
+  g.fillRoundedRect(36, 74, 14, 9, 3);
+  g.fillRoundedRect(46, 74, 14, 9, 3);
+  g.fillStyle(shortsShade, 1);
+  g.fillRoundedRect(36, 79, 24, 4, 2);
 
   // Legs + shoes.
-  g.fillStyle(pants, 1);
-  g.fillRoundedRect(39 - legOffset, 71, 8, 11, 3);
-  g.fillRoundedRect(49 + legOffset, 71, 8, 11, 3);
-  g.fillStyle(pantsShade, 1);
-  g.fillRoundedRect(39 - legOffset, 75, 8, 7, 3);
-  g.fillRoundedRect(49 + legOffset, 75, 8, 7, 3);
+  g.fillStyle(skin, 1);
+  g.fillRoundedRect(39 - legOffset, 82, 8, 7, 3);
+  g.fillRoundedRect(49 + legOffset, 82, 8, 7, 3);
   g.fillStyle(0x1b1b1b, 1);
-  g.fillRoundedRect(37 - legOffset, 80, 10, 6, 3);
-  g.fillRoundedRect(49 + legOffset, 80, 10, 6, 3);
+  g.fillRoundedRect(36 - legOffset, 87, 12, 7, 4);
+  g.fillRoundedRect(48 + legOffset, 87, 12, 7, 4);
 
   // Face details.
-  if (direction !== 'back') {
+  if (!facingBack) {
     g.fillStyle(0x3a2b2a, 1);
-    if (direction === 'side') {
-      g.fillEllipse(52 + stepX, 33, 4, 5);
+    if (facingSide) {
+      g.fillEllipse(53 + stepX, 33, 4, 6);
     } else {
-      g.fillEllipse(42 + stepX, 33, 4, 5);
-      g.fillEllipse(54 + stepX, 33, 4, 5);
+      g.fillEllipse(42 + stepX, 33, 4, 6);
+      g.fillEllipse(54 + stepX, 33, 4, 6);
     }
 
     // Blush + mouth.
     g.fillStyle(0xf8c29f, 0.58);
-    g.fillCircle(38, 39, 3.5);
-    g.fillCircle(58, 39, 3.5);
+    if (!facingSide) {
+      g.fillCircle(38, 39, 3.5);
+      g.fillCircle(58, 39, 3.5);
+    }
 
     g.lineStyle(2, 0xd68062, 0.95);
     g.beginPath();
-    g.arc(48 + stepX, 40, 2.8, 0.2, Math.PI - 0.2, false);
+    g.arc(facingSide ? 47 : 48 + stepX, 40, 2.8, 0.2, Math.PI - 0.2, false);
     g.strokePath();
   }
 
   // Soft outline pass for chibi readability.
   g.lineStyle(2, outline, 0.82);
-  g.strokeCircle(48, 29, 21);
-  g.strokeRoundedRect(33, 48, 30, 24, 8);
-  g.strokeRoundedRect(37 - legOffset, 80, 22, 6, 3);
+  g.strokeCircle(48, 30, 21);
+  g.strokeRoundedRect(32, 49, 32, 23, 8);
+  g.strokeRoundedRect(34, 69, 28, 5, 2);
+  g.strokeRoundedRect(36 - legOffset, 87, 24, 7, 4);
 
   g.generateTexture(key, w, h);
   g.destroy();
@@ -167,7 +188,7 @@ export default class HumanChibiAvatar extends SpriteAvatarBase {
     super(scene, x, y, {
       ...options,
       frameKeys,
-      targetHeight: 72,
+      targetHeight: 74,
       shadowColor: 0x000000,
     });
   }
